346d1b17350a61032ff7969c04ddfaa57bd36adb
rishabhkr-r111 <rishabhkrr111@gmail.com>
2022-11-09 21:20:28 +0530

    cros enable

--- index.py
+++ index.py
@@ -3,9 +3,11 @@ import json
 import lxml
 from bs4 import BeautifulSoup
 from flask import Flask,jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def main():
